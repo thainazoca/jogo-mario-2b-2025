095e98bfdde55708d2fbcccb1d91427da5914758
thainazoca <thaina.barbosa.santos@escola.pr.gov.br>
2025-06-13 10:26:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+const mario = document.querySelector(".mario")
+const pipe = document.querySelector(".pipe)
+                                    
+const jump = () => {
+  mario.classList.remove.add("jump);
 
+ seTimeout(() => {
+    mario.classList.remove("Jump");
+  }
+           ,500);
+}
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  cobst marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+
+                         if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+                           pipe.style.animation = "none";
+                           pipe.style.left = ${pipePosition}px`;
+
+                           mario.style.animation = "none";
+                           mario.style.left = `${marioPosition}px`;
+                           }
+                           })
+                           document.addEventListener("keydown", jump);
